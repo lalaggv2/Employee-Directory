b20162f98365ae3c3af14968abbe2c6dacabae65
Lala <luangu2020@gmail.com>
2020-08-09 13:08:58 -0400

    Updates

---
+++ precache-manifest.1e4f99a66b5bd3bc086721ea7806932b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a144d84dc45a7443221c33f7ed98e007",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "83823d80a8acfae7fba5",
+    "url": "/Employee-Directory/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d0cc9401b7f0eedacc3b",
+    "url": "/Employee-Directory/static/css/main.8990d488.chunk.css"
+  },
+  {
+    "revision": "83823d80a8acfae7fba5",
+    "url": "/Employee-Directory/static/js/2.146dcc0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.146dcc0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0cc9401b7f0eedacc3b",
+    "url": "/Employee-Directory/static/js/main.cc8f7810.chunk.js"
+  },
+  {
+    "revision": "52e1e8e23e7dfaf64514",
+    "url": "/Employee-Directory/static/js/runtime-main.6ca9f082.js"
+  }
+]);
