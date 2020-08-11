fd51ac1550cdf81d26e49c95c419bd3eec4fff0f
Lala <luangu2020@gmail.com>
2020-08-10 23:00:39 -0400

    sorting by name, css changes

--- employeedirectory/src/components/Search-form.js
+++ employeedirectory/src/components/Search-form.js
@@ -4,29 +4,19 @@ import React from "react";
 function SearchForm(props) {
   return (
     <div className="uk-container form-container">
-      {/* <form> */}
-      <div className="uk-margin">
-        <label>Search Employee by Name:</label><br />
+
+      <div className="search">
+        <label>Search Employee by Name:</label>
         <input
           onChange={props.filter}
           className="input-box uk-input uk-form-width-medium" type="text"
           placeholder="Employee Name"
         />
       </div>
-      {/* </form> */}
+
     </div>
   )
 };
-
-// handleInputChange = event => {
-//   // Getting the value and name of the input which triggered the change
-//   const { name, value } = event.target;
-
-//   // Updating the input's state
-//   this.setState({
-//     [name]: value
-//   });
-// };
 
 
 export default SearchForm;
